56a570711b16389713cc6625be4089e6cd9b2836
Sarah Vessels <cheshire137@gmail.com>
2010-10-29 15:24:34 -0400

    Adding initial grammar that grabs everything between two non-outputting ERB tags and returns an array with :erb_tag in it.  Adding generator that just parses a hard-coded string of ERB currently.

---
+++ asm/generator.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'treetop'
+require 'polyglot'
+require 'erb_grammar'
+require 'pp'
+
+unless ARGV.length == 1
+  printf("Usage: %s path_to_erb_file\n", $0)
+  exit
+end
+
+erb = "<% whee %>"
+parser = ERBGrammarParser.new
+parse_result = parser.parse(erb)
+
+if parse_result.nil?
+  puts "Error:  could not parse the following ERB: "
+  puts erb
+  exit
+end
+
+pp parse_result.content
+
+=begin
+erb = IO.readlines(ARGV.first).join
+parser = ERBGrammarParser.new
+parse_result = parser.parse(erb)
+=end
