ed582933fdbda7c5f4de201688402c01c6502db7
Sarah Vessels <cheshire137@gmail.com>
2010-11-05 18:10:03 -0400

    Adding ability to save a scraped site to a YAML file.  Will later add ability to read from that YAML file instead of re-scraping a site, to save time.

--- qmz/scraper.rb
+++ qmz/scraper.rb
@@ -4,14 +4,31 @@ require 'pfd.rb'
 require 'page.rb'
 require 'site.rb'
 
-unless ARGV.length.eql? 1
-  printf("Usage: %s uri_to_site_home_page\n", $0)
-  exit
+def print_usage(app_name)
+  printf("Usage: %s uri_to_site_home_page [file_to_save_site_structure]\n",
+    app_name)
 end
 
-home = Page.new(ARGV.first)
+if ARGV.length < 1
+  print_usage($0)
+  exit
+elsif ARGV.length > 2
+  print_usage($0)
+  printf("WARN: ignoring last %d argument(s)\n", ARGV.length - 2)
+end
+
+home = Page.new(ARGV.shift)
 site = Site.new(home)
-printf("\n%s\n\n", site.to_s)
+printf("\n%s\n", site.to_s)
+unless ARGV.empty?
+  path = ARGV.shift
+  printf("\nWriting site to %s...\n", path)
+  File.open(path, 'w') do |file|
+    file.puts YAML::dump(site)
+  end
+  printf("File successfully written\n")
+end
+print "\n"
 pfd = site.get_pfd
 ptt = Site.pfd2ptt(pfd)
 test_paths = ptt.get_test_paths
