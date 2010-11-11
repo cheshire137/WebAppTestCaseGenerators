53ef3e21b098d470b8c266e0bfc7508fd10b8639
Sarah Vessels <cheshire137@gmail.com>
2010-11-10 22:41:34 -0500

    Incorporating more of rhtml.treetop into my ERB grammar, including rules text, document, node.  Altering rules erb, erb_output to allow whitespace. Changing ruby_code rule to be like rhtml.treetop's rule.  Altering whitespace rule to allow for any number of whitespace bits.  Renaming attribute_list to html_tag_attributes.  Renaming attribute to html_tag_attribute.  Using rhtml.treetop's quoted_value rule, even though trying to put <HTMLQuotedValue> after it like the other rules causes an error--something weird with all the quotes?

--- asm/erb_node_extensions.rb
+++ asm/erb_node_extensions.rb
@@ -1,49 +1,115 @@
 module ERBGrammar
   class ERBDocument < Treetop::Runtime::SyntaxNode
     def content
-      elements.collect do |node|
-        if node.respond_to? :content
-          node.content
-        else
-          node.inspect
-        end
+      if x.empty?
+        [node.content]
+      else
+        [node.content] + x.content
       end
     end
   end
 
   class ERBOutputTag < Treetop::Runtime::SyntaxNode
     def content
-      [:erb_output, elements[1].content]
+      [:erb_output, ruby_code.content_removing_trims]
     end
   end
 
   class ERBTag < Treetop::Runtime::SyntaxNode
     def content
-      [:erb, elements[1].content]
-    end
-  end
-
-  class HTMLTagPair < Treetop::Runtime::SyntaxNode
-    def content
-      [:html_tag_pair, elements.inspect]
+      [:erb, ruby_code.text_value_removing_trims.strip]
     end
   end
 
   class HTMLOpenTag < Treetop::Runtime::SyntaxNode
     def content
-      [:html_open_tag, elements[1].content, elements[2].content]
+      [:html_open_tag, tag_name.content, attrs.empty? ? "" : attrs.content]
     end
   end
 
   class HTMLCloseTag < Treetop::Runtime::SyntaxNode
     def content
-      [:html_close_tag, elements[1].content]
+      [:html_close_tag, tag_name.content]
     end
   end
 
   class HTMLSelfClosingTag < Treetop::Runtime::SyntaxNode
     def content
-      [:html_self_closing_tag, tag_name.text_value, attrs.empty? ? "" : attrs.content]
+      [:html_self_closing_tag, tag_name.content, attrs.empty? ? "" : attrs.content]
+    end
+  end
+
+  class HTMLTagAttributes < Treetop::Runtime::SyntaxNode
+    def content
+      [:html_tag_attributes, [head.content] + (tail.empty? ? [] : tail.elements.first.content.last)]
+    end
+  end
+
+  class HTMLTagAttribute < Treetop::Runtime::SyntaxNode
+    def attr_name
+      (n.text_value =~ /[-:]/) ? "'#{n.text_value}'" : ":#{n.text_value}"
+    end
+
+    def content
+      [:html_tag_attribute, attr_name, v.content]
+    end
+
+    def to_s
+      "#{attr_name} => #{v.content}"
+    end
+  end
+
+  class HTMLQuotedValue < Treetop::Runtime::SyntaxNode
+    def content
+      [:quoted_value, val.text_value]
+    end
+  
+    def convert
+      extract_erb(val.text_value)
+    end
+    
+    def parenthesize_if_necessary(s)
+      return s if s.strip =~ /^\(.*\)$/ || s =~ /^[A-Z0-9_]*$/i
+      "(" + s + ")"
+    end
+    
+    def extract_erb(s, parenthesize = true)
+      if s =~ /^(.*?)<%=(.*?)%>(.*?)$/
+        #pre, code, post = $1.html_unescape.escape_single_quotes, $2, $3.html_unescape.escape_single_quotes
+        pre, code, post = $1, $2, $3
+        out = ""
+        out = "'#{pre}' + " unless pre.length == 0
+        out += parenthesize_if_necessary(code.strip)
+        unless post.length == 0
+          post = extract_erb(post, false)
+          out += " + #{post}"
+        end
+        out = parenthesize_if_necessary(out) if parenthesize
+        out
+      else
+        #"'" + s.html_unescape.escape_single_quotes + "'"
+        "'" + s + "'"
+      end
+    end
+  end
+
+  class RubyCode < Treetop::Runtime::SyntaxNode
+    def content_removing_trims
+      result.gsub(/\s*\-\s*$/, '')
+    end
+    
+    def text_value_removing_trims
+      text_value.gsub(/\s*\-\s*$/, '')
+    end
+    
+    def content
+      [:ruby_code, result]
+    end
+
+    def result
+      code = text_value.strip
+      # matches a word, followed by either a word, a string, or a symbol
+      code.gsub(/^(\w+) ([\w:"'].*)$/, '\1(\2)')
     end
   end
 end
